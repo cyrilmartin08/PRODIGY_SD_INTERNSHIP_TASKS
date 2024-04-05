d1b026b4a006bb1a8f8f48611ef4bf0bba99c667
cyrilmartin08 <152998847+cyrilmartin08@users.noreply.github.com>
2024-04-05 12:58:18 +0530

    Create PRODIGY_SD_04.py

---
+++ PRODIGY_SD_04.py
@@ -0,0 +1,66 @@
+def is_valid_move(board, row, col, num):
+    # Check if num is already in the row
+    if num in board[row]:
+        return False
+
+    # Check if num is already in the column
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+
+    # Check if num is already in the 3x3 box
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(start_row, start_row + 3):
+        for j in range(start_col, start_col + 3):
+            if board[i][j] == num:
+                return False
+
+    return True
+
+def solve_sudoku(board):
+    empty_cell = find_empty_cell(board)
+    if not empty_cell:
+        return True  # Puzzle is solved
+    row, col = empty_cell
+
+    for num in range(1, 10):
+        if is_valid_move(board, row, col, num):
+            board[row][col] = num
+            if solve_sudoku(board):
+                return True
+            board[row][col] = 0  # Undo the move
+
+    return False
+
+def find_empty_cell(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+def print_board(board):
+    for row in board:
+        print(" ".join(map(str, row)))
+
+# Example Sudoku puzzle (0 represents empty cells)
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+print("Input Sudoku puzzle:")
+print_board(board)
+
+if solve_sudoku(board):
+    print("\nSolved Sudoku puzzle:")
+    print_board(board)
+else:
+    print("\nNo solution exists for the Sudoku puzzle.")
